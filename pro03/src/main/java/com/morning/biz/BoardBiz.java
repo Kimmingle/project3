1880e82775e25886c0472b7cacb4fd1286f090f8
Kimmingle <t5328@naver.com>
2024-05-29 17:52:40 +0900

    commit

--- pro03/src/main/java/com/morning/biz/BoardBiz.java
+++ pro03/src/main/java/com/morning/biz/BoardBiz.java
@@ -7,10 +7,12 @@ import com.morning.domain.Board;
 public interface BoardBiz {
 	public int getTotalCount();
 	public List<Board> getBoardList();
-	public Board getBoard(int bno);
-	public Board selBoard(int bno);
+	public Board getBoard(int bno);   
+	public Board selBoard(int bno);   //게시물만 조회하는 용도
 	public void insBoard(Board board);
 	public void upBoard(Board board);
 	public void countUpCnt(int bno);
 	public void delBoard(int bno);
 }
+
+
--- pro03/src/main/java/com/morning/per/BoardMapper.java
+++ pro03/src/main/java/com/morning/per/BoardMapper.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import com.morning.domain.Board;
 
-public interface BoardMapper {
+public interface BoardMapper {  //마이바티스 사용해서 sql실행
 	
 	public int getTotalCount();
 	public List<Board> getBoardList();
 	public Board getBoard(int bno);
+	//public Board selBoard(int bno);  //get이랑 겹쳐서?
 	public void insBoard(Board board);
 	public void upBoard(Board board);
 	public void vcntCount(int bno);
 	public void delBoard(int bno);
 }
+
+
+
